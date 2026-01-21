2ca1bbc909131595f4a708784ef8dc8b63081253
yungblud <killi8n@gmail.com>
2026-01-21 22:19:03 +0900

    feat(official-blog): init sst

---
+++ apps/official-blog/sst.config.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+const name = 'official-blog';
+
+export default $config({
+  app(input) {
+    return {
+      name,
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs(name);
+  },
+});
