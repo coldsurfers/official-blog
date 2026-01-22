c818476add2780e161f1dcda4d5299c1403f7991
yungblud <killi8n@gmail.com>
2026-01-22 09:28:50 +0900

    feat(official-blog): enhance tags page

--- apps/official-blog/src/app/tags/page.tsx
+++ apps/official-blog/src/app/tags/page.tsx
@@ -3,9 +3,6 @@ import { PageLayout } from '@/ui/page-layout';
 import { TagList } from '@/ui/tag-list';
 import { RouteLoading } from '@coldsurfers/ocean-road/next';
 
-export const dynamic = 'force-static';
-export const revalidate = 3600;
-
 export default async function TagsPage() {
   const data = await fetchTags();
   const tags = data.tags;
